576696e82947f986846427f7a6480be6825238b7
Sungho Hong <sunghohong@ibs.re.kr>
2025-08-14 16:53:55 +0900

    remove external dependency in the base script

--- _start_ipcluster.sh
+++ _start_ipcluster.sh
@@ -12,10 +12,6 @@ echo 'WORKDIR =' $SLURM_SUBMIT_DIR
 NENGS=$(($SLURM_NTASKS - 1))
 echo 'NENGS = ' $NENGS
 
-module load python/3.10.2
-module load openmpi.gcc
-alias python='python3'
-
 IPYTHONDIR=$SLURM_SUBMIT_DIR/$SLURM_JOB_ID.ipython #Create an ipython configuration directory
 echo $IPYTHONDIR
 mkdir -p $IPYTHONDIR
